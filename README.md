e0a0130a61ae518721dca659b2811dc077bd49e1
Mateusz <mateusz.lukasik@student.pk.edu.pl>
2026-03-03 10:29:41 +0100

    Font changes and fixed some small issues

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # text-editor
-Simple text editor made by me for me 
+A lightweight, Sublime Text-inspired code editor built with Python and Tkinter. This editor uses the Fira Code monospace.
+
+Installation
+
+    1.Clone the repository:
+
+    Bash
+
+    git clone https://github.com/your-username/your-repo-name.git
+    cd your-repo-name
+
+    2. Install dependencies:
+    This project requires the tkextrafont library to load the Fira Code font without requiring the user to install it on their system.
+
+    Bash
+
+    pip install -r requirements.txt
+
+    3.Ensure the font file is present:
+    Make sure FiraCode-Light.ttf is in the same folder as text_editor.py.
--- text_editor.py
+++ text_editor.py
@@ -1,4 +1,4 @@
-
+from tkextrafont import Font
 from tkinter import filedialog
 from tkinter import *
 
@@ -11,13 +11,20 @@ def newFile():
 
 def saveFile():
 	global filename
-	t = text.get(0.0,END)
-	f = open(filename, 'w')
-	f.write(t)
+	if filename is None:
+		saveAs()
+		return
+	try:
+		t = text.get(0.0,END)
+		with open(filename, 'w') as f:
+			f.write(t)
+	except Exception as e:
+		print(f"ERROR SAVING {e}")
+	
 	f.close()
 
 def saveAs():
-	f = asksaveasfile(mode='w', defaultextension='.txt')
+	f = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
 	t = text.get(0.0, END)
 	try:
 		f.write(t.rstrip())
@@ -25,10 +32,14 @@ def saveAs():
 		showerror(title="ERROR", message = "Unabnle to save file...")
 
 def openFile():
-	f = askopenfile(mode='r')
-	t = f.read()
-	text.delete(0.0, END)
-	text.insert(0.0, t)
+	f = filedialog.askopenfile(mode='r')
+	if f is not None:
+		try:
+			t = f.read()
+			text.delete(0.0, END)
+			text.insert(0.0, t)
+		finally:
+			f.close()
 
 root = Tk()
 root.title("My Text Editor")
@@ -36,8 +47,12 @@ root.minsize(width=800,height=600)
 root.maxsize(width=400,height=400)
 root.configure(bg="#343567")
 
-text = Text(root, width=400, height=400, bg="#1f1f3d", fg="#ffffff", insertbackground="white")
-text.pack(padx=2, pady=2)
+fira_font = Font(file="FiraCode-Light.ttf", family="Fira Code")
+text = Text(root, width=400, height=400, bg="#1f1f3d", fg="#ffffff",spacing1=4, 
+               spacing2=2, insertbackground="white")
+text.pack(padx=2, pady=2, expand=True, fill="both")
+text.configure(font=(fira_font, 8))
+
 
 menubar = Menu(root)
 filemenu = Menu(menubar)
